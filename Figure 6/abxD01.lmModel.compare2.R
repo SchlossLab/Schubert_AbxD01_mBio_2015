3d9d63e01b122ae4d30bb6d46efff3649badf800
Alyx Schubert <alyx@umich.edu>
2014-12-04 16:08:27 -0500

    realized i picked the 5 model incorrectly
    pick the 3 with optional 4/5

--- Figure 6/abxD01.lmModel.compare2.R
+++ Figure 6/abxD01.lmModel.compare2.R
@@ -26,3 +26,66 @@ leapss<-regsubsets(nextDayCFU ~ Otu00002 + Otu00003 + Otu00006 + Otu00007 + Otu0
 summary.leaps<-summary(leaps)
 which.max(summary.leaps$adjr2) #outputted 28, which is model number
 summary.leaps$which[28,]
+
+lm5<-lm(nextDayCFU ~ Otu00006 + Otu00007 + Otu00020 + Otu00039 + Otu00283, data=td)
+summary(lm5)
+
+summary.leapss<-summary(leapss)
+which.max(summary.leapss$adjr2) 
+summary.leapss$which[26,]
+
+lmadjr2<-lm(nextDayCFU ~ Otu00002 + Otu00006 + Otu00007 + Otu00013 + Otu00015 + Otu00020 + Otu00023 + Otu00039 + Otu00120 + Otu00283, data=td)
+summary(lmadjr2)
+
+
+
+####################
+##testing the model with 5 OTUs against the new titration
+lm5<-lm(nextDayCFU ~ Otu00006 + Otu00007 + Otu00020 + Otu00039 + Otu00283, data=td)
+summary(lm5)
+newtit2<-read.csv("~/Desktop/mothur/abxD01/model/abxD01.final.an.unique_list.0.03.subsample.shared.lm.newtitration.logtrans.filter16mintotal.19otus.csv", header=T)
+newtit2<-newtit2[,-1]
+actual<-as.data.frame(newtit2[,1])
+newtit<-newtit2[,-1]
+predictlm5 <- as.data.frame(predict.lm(lm5, newdata=newtit))
+
+ybar = colMeans(actual)[1]
+SStot = sum((actual-ybar)^2)
+
+SSres = sum((actual-predictlm5)^2)
+rsq = 1-(SSres/SStot)
+rsq
+
+
+####################
+##testing the best model against the new titration
+lmadjr2<-lm(nextDayCFU ~ Otu00002 + Otu00006 + Otu00007 + Otu00013 + Otu00015 + Otu00020 + Otu00023 + Otu00039 + Otu00120 + Otu00283, data=td)
+
+newtit2<-read.csv("~/Desktop/mothur/abxD01/model/abxD01.final.an.unique_list.0.03.subsample.shared.lm.newtitration.logtrans.filter16mintotal.19otus.csv", header=T)
+newtit2<-newtit2[,-1]
+actual<-as.data.frame(newtit2[,1])
+newtit<-newtit2[,-1]
+predictlmadjr2 <- as.data.frame(predict.lm(lmadjr2, newdata=newtit))
+
+ybar = colMeans(actual)[1]
+SStot = sum((actual-ybar)^2)
+
+SSres = sum((actual-predictlmadjr2)^2)
+rsq = 1-(SSres/SStot)
+rsq
+
+####################
+##try leaving out OTU7 or OTU6 because they have the highest correlation with each other
+leapsno7<-regsubsets(nextDayCFU ~ Otu00002 + Otu00003 + Otu00006 + Otu00011 + Otu00013 + Otu00015 + Otu00019 + Otu00020 + Otu00023 + Otu00027 + Otu00029 + Otu00039 + Otu00044 + Otu00065 + Otu00078 + Otu00120 + Otu00283 + Otu00431, data=td, nbest=3, nvmax=10)
+summary(leapsno7)
+plot(leapsno7,scale="bic")
+leapssno7<-regsubsets(nextDayCFU ~ Otu00002 + Otu00003 + Otu00006 + Otu00011 + Otu00013 + Otu00015 + Otu00019 + Otu00020 + Otu00023 + Otu00027 + Otu00029 + Otu00039 + Otu00044 + Otu00065 + Otu00078 + Otu00120 + Otu00283 + Otu00431, data=td, nbest=3, nvmax=10, method="seqrep")
+plot(leapssno7,scale="bic")
+
+leapsno6<-regsubsets(nextDayCFU ~ Otu00002 + Otu00003 + Otu00007 + Otu00011 + Otu00013 + Otu00015 + Otu00019 + Otu00020 + Otu00023 + Otu00027 + Otu00029 + Otu00039 + Otu00044 + Otu00065 + Otu00078 + Otu00120 + Otu00283 + Otu00431, data=td, nbest=3, nvmax=10)
+summary(leapsno6)
+plot(leapsno6,scale="bic")
+leapssno6<-regsubsets(nextDayCFU ~ Otu00002 + Otu00003 + Otu00007 + Otu00011 + Otu00013 + Otu00015 + Otu00019 + Otu00020 + Otu00023 + Otu00027 + Otu00029 + Otu00039 + Otu00044 + Otu00065 + Otu00078 + Otu00120 + Otu00283 + Otu00431, data=td, nbest=3, nvmax=10, method="seqrep")
+plot(leapssno6,scale="bic")
+
+
