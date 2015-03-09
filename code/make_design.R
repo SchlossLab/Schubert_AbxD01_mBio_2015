cf8e9ff5ba1e285bfb79a3a0bd97ff4e44139f9e
Pat Schloss <pschloss@umich.edu>
2015-03-09 14:08:17 -0400

    Initial commit

---
+++ code/make_design.R
@@ -0,0 +1,16 @@
+################################################################################
+#
+# make_design.R
+#
+# This script will generate the various design files that are needed to run the
+# amova command from within mothur
+#
+# Dependencies...
+# * The shared file that will be used in the amova command
+# * The table that maps the sample names to the treatments (data/raw/abxD01_IDS.txt)
+# * Information about the type of design file to make.
+#
+# Output...
+# * The relevant design file
+#
+################################################################################
