ae68b17767970734a7e95ede9aacf38413dad15d
Pat Schloss <pschloss@umich.edu>
2015-03-10 08:01:59 -0400

    Rough-in reading input files

--- code/make_design.R
+++ code/make_design.R
@@ -14,3 +14,12 @@
 # * The relevant design file
 #
 ################################################################################
+
+mapping_file <- "data/raw/abxD01_IDS.txt"
+mapping_data <- read.table(file=mapping_file, header=T)
+day_zero <- mapping_data[mapping_data$day==0,]
+
+shared_file <- "data/process/abxD0.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.an.unique_list.0.03.subsample.shared"
+shared_data <- read.table(file=shared_file, row.names=2, header=T)
+shared_data <- shared_data[,-c(1,2)]
+samples <- rownames(shared_data)
