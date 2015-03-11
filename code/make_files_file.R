ad44938df957fa62109da55305c05f354a3839aa
Pat Schloss <pschloss@umich.edu>
2015-03-11 15:16:16 -0400

    Initial commit

---
+++ code/make_files_file.R
@@ -0,0 +1,49 @@
+################################################################################
+#
+# make_files_file.R
+#
+# This script will build the files file that will be used in the make.contigs
+# command from within mothur.
+#
+# Dependencies...
+# * data/process/abx_cdiff_metadata.tsv
+# * fastq files stored in ../../cdiff_fastqs/
+#
+# Output...
+# * data/process/abxD0.files
+#
+################################################################################
+
+metadata <- read.table(file="data/process/abx_cdiff_metadata.tsv", header=T)
+
+#did the sample come from a mouse that eventually received Cdiff?
+cdiff_challenged <- metadata[metadata$cdiff==TRUE,]
+
+#get the day 0 samples from the abx treated mice
+abx_treated <- cdiff_challenged[cdiff_challenged$day == 0, ]
+
+#get the pre-abx samples from mice that would receive abx
+untreated <-  cdiff_challenged[cdiff_challenged$preAbx & cdiff_challenged$abx != "none", ]
+
+#concatenate the sample names together
+samples <- c(rownames(abx_treated), rownames(untreated))
+
+#get the list of fastq file names
+fastqs <- list.files("../../cdiff_fastqs/", pattern="*.fastq")
+
+
+#need a function that will build the files file line for each stub
+find_files <- function(stub, files=fastqs){
+    pattern <- paste0(stub, ".*R1.*fastq")  #use the stub to create a search pattern
+    r1_file <- files[grep(pattern, files)]  #find all of the possible R1 files
+    r2_file <- gsub("R1", "R2", r1_file)    #find all of the possible R2 files
+    label <- rep(stub, length(r1_file))     #make a vector of the sample name
+
+    #this all is necessary because some samples were sequenced multiple times
+    #the output of the above will be a series of vectors that here we will bind
+    #as columns, and paste across the columns for each row
+    apply(cbind(label, r1_file, r2_file), 1, paste, collapse="\t")
+}
+
+files_lines <- lapply(samples, find_files)
+write(unlist(files_lines), "data/process/abxD0.files")
