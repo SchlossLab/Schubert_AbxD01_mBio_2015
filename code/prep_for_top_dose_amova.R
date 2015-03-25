03ec29af1962ae5514e1176d683b9e67b33c6c8d
Pat Schloss <pschloss@umich.edu>
2015-03-25 11:52:22 -0400

    Make use of data/process/abxD1.counts file

--- code/prep_for_top_dose_amova.R
+++ code/prep_for_top_dose_amova.R
@@ -7,7 +7,7 @@
 # structures are different between the different antibiotic treatment groups
 #
 # Dependencies...
-# * data/process/abx_cdiff_metadata.tsv
+# * data/process/abxD1.counts
 # * data/process/abxD0.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.an.unique_list.shared
 #
 # Output...
@@ -18,7 +18,7 @@
 
 
 #get the metadata file that was generated earlier...
-metadata_file <- "data/process/abx_cdiff_metadata.tsv"
+metadata_file <- "data/process/abxD1.counts"
 metadata_data <- read.table(file=metadata_file, header=T)
 
 
@@ -27,25 +27,17 @@ shared_file <- "data/process/abxD0.trim.contigs.good.unique.good.filter.unique.p
 shared_data <- read.table(file=shared_file, header=T, row.names=2)
 shared_data <- shared_data[,-c(1,2)]
 
-#anticipate which samle will be excluded because there aren't enough reads
+#anticipate which sample will be excluded because there aren't enough reads
 enough_seqs <- apply(shared_data, 1, sum) >= 1625
 shared_data_enough <- shared_data[enough_seqs,]
 
 #get the sample names so we can pull those samples out of the metadata file
 day_zero_metadata <- metadata_data[rownames(shared_data_enough),]
 
-#exclude the stuff from the delayed experiments
-no_delay_metadata <- day_zero_metadata[!day_zero_metadata$delayed,]
 
-#exclude the stuff from the titration experiments
-top_dose <- which(no_delay_metadata$abx == "amp")
-top_dose <- c(top_dose, which(no_delay_metadata$abx == "cipro"))
-top_dose <- c(top_dose, which(no_delay_metadata$abx == "clinda"))
-top_dose <- c(top_dose, which(no_delay_metadata$abx == "metro"))
-top_dose <- c(top_dose, which(no_delay_metadata$abx == "cef" & no_delay_metadata$dose == "0.5"))
-top_dose <- c(top_dose, which(no_delay_metadata$abx == "strep" & no_delay_metadata$dose == "5"))
-top_dose <- c(top_dose, which(no_delay_metadata$abx == "vanc" & no_delay_metadata$dose == "0.625"))
-top_dose_metadata <- no_delay_metadata[top_dose,]
+#get the top_dose data
+top_dose <- day_zero_metadata$experiment=="top_dose" | day_zero_metadata$abx=="control"
+top_dose_metadata <- day_zero_metadata[top_dose,]
 
 #get those sample names
 top_dose_samples <- rownames(top_dose_metadata)
@@ -54,7 +46,6 @@ top_dose_samples <- rownames(top_dose_metadata)
 #output a design file where the pre-antibiotic samples are the controls
 top_dose_design_file <- "data/process/abx_topdose.design"
 treatment <- top_dose_metadata$abx
-treatment[top_dose_metadata$day < 0] <- "no_abx"
 write.table(cbind(top_dose_samples, treatment), file=top_dose_design_file, quote=F, col.names=F, row.names=F, sep="\t")
 
 
