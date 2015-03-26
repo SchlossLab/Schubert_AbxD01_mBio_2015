a1acf962392111c799531a912ade3ca8df64674c
Pat Schloss <pschloss@umich.edu>
2015-03-26 13:08:34 -0400

    Output rownames to table

--- code/build_figure2.R
+++ code/build_figure2.R
@@ -129,4 +129,4 @@ dev.off()
 
 
 # output the correlation data
-write.table(file="data/process/top_dose_corr.tsv", cbind(taxonomy=gsub("\\d", "", sig_taxonomy), sig_corrs), quote=F, row.names=F, sep="\t")
+write.table(file="data/process/top_dose_corr.tsv", cbind(taxonomy=gsub("\\d", "", sig_taxonomy), sig_corrs), quote=F, row.names=T, sep="\t")
