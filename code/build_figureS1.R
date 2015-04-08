594b9516d0365ca90bcfa0fe84356b4536352113
Pat Schloss <pschloss@umich.edu>
2015-04-08 13:32:33 -0400

    Fix output file name and alpha the antibiotics

--- code/build_figureS1.R
+++ code/build_figureS1.R
@@ -89,7 +89,7 @@ fulldose_phylum_barplot <- function(drug, label){
     pos
 }
 
-pdf(height=9, width=3.75, file="results/figures/figure1.pdf")
+pdf(height=9, width=3.75, file="results/figures/figureS1.pdf")
 
     z <- layout(
         matrix( c(  1,
@@ -106,16 +106,16 @@ pdf(height=9, width=3.75, file="results/figures/figure1.pdf")
     par(mar=c(0.5, 5, 1.25, 0.5))
 
     pos <- fulldose_phylum_barplot("control", "No antibiotics")
-    pos <- fulldose_phylum_barplot("cipro", "Ciprofloxacin")
-    pos <- fulldose_phylum_barplot("vanc", "Vancomycin")
     pos <- fulldose_phylum_barplot("amp", "Ampicillin")
+    pos <- fulldose_phylum_barplot("cef", "Cefoperazone")
+    pos <- fulldose_phylum_barplot("cipro", "Ciprofloxacin")
 
     mtext(side=2, line=3, "Relative Abundance (%)")
 
     pos <- fulldose_phylum_barplot("clinda", "Clindamycin")
-    pos <- fulldose_phylum_barplot("strep", "Streptomycin")
-    pos <- fulldose_phylum_barplot("cef", "Cefoperazone")
     pos <- fulldose_phylum_barplot("metro", "Metronidazol")
+    pos <- fulldose_phylum_barplot("strep", "Streptomycin")
+    pos <- fulldose_phylum_barplot("vanc", "Vancomycin")
     text(x=pos+0.1, y=par("usr")[3]-10, labels=rownames(med_ra), srt=70, cex=1, font=1, pos=2, xpd=NA)
 
 dev.off()
